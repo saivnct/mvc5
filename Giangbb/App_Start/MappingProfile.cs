2fd6a8377e1a2368d732c1cef9e6da4ad9524a6d
giangbb <giangbb@viettel.com.vn>
2018-07-09 18:50:30 +0700

    add api

---
+++ Giangbb/App_Start/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Giangbb.Dtos;
+using Giangbb.Models;
+
+namespace Giangbb.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Customer, CustomerDto>());
+        }
+    }
+}
